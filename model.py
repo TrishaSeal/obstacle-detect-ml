9983008345652833721161ce9f360fe211565fb4
DarshK35 <darsh.kumar2002@gmail.com>
2023-06-25 15:25:21 +0530

    Modified model.py

--- model.py
+++ model.py
@@ -1,2 +1,21 @@
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+# Import model
+import tensorflow as tf
+imported = tf.saved_model.load('./export-model/')
+model_fn = imported.signatures['serving_default']
+
+def createRecord(imageDir, annotPath, recordPath):
+	cmd = [
+		"python3 -m official.vision.data.create_coco_tf_record ",
+		"--logtostderr ",
+		"--image_dir=\"" + imageDir + "\" ",
+		"--object_annotations_file=\"" + annotPath + "\" ",
+		"--output_file_prefix=\"" + recordPath + "\" ",
+		"--num_shards=1"
+	]
+	fullcmd = ""
+	for c in cmd:
+		fullcmd += c
+	os.system(fullcmd)
